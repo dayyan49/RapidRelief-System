78d92f9c870a501ab3558f364c2a3cd596831959
Adityaverma18 <adityaverma20111@gmail.com>
2026-06-12 22:42:55 +0530

    Deploying the site

--- backend/Node_backend/src/config/env.js
+++ backend/Node_backend/src/config/env.js
@@ -6,7 +6,7 @@ export const ENV = {
   PORT: process.env.PORT || 3000,
   MONGODB_URI: process.env.MONGODB_URI ,
   JWT_SECRET: process.env.JWT_SECRET || "83qq982$@kuq",
-  PYTHON_SERVICE_URL: process.env.PYTHON_SERVICE_URL || "http://localhost:5001",
+  PYTHON_SERVICE_URL: process.env.PYTHON_SERVICE_URL ,
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
